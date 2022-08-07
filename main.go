ceeb7f97f8e6e9c6b1b8543f15d400a5de1f4412
tetsu.NAKAMURA <eng.nakamura.tetsu@gmail.com>
2022-08-07 15:53:09 +0900

    add main test

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func run(ctx context.Context) error {
 	s := http.Server{
 		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Heelo, %s", r.URL.Path[1:])
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
 
